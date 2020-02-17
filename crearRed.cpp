21ea898017aaed282284a4f9cbcd0c6e04955218
Jose Soto <josesotogarci@gmail.com>
2020-02-17 18:52:15 +0100

    programa redes aleatorias funciona perfectamente

---
+++ crearRed.cpp
@@ -0,0 +1,63 @@
+//Libraries
+#include<iostream>
+#include<cstdlib>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include<vector>
+
+using namespace std;
+
+
+int main(void)
+{
+    int N;
+    int i, j, tamano;
+    int conectividad;
+    vector< vector<int> > conexion;
+    N = 10;
+    for (i = 0; i<N; i++)
+    {
+        conexion.push_back(vector<int>());
+    }
+
+
+
+    conectividad = 50;
+
+     //Generador de aleatorios
+    random_device rd;
+    mt19937 gen(45454536345331);
+    uniform_real_distribution<> dis(0, 100);
+
+    for (i=0;i<N;i++)
+        {
+            for(j=i+1;j<N;j++)
+            {
+               if (dis(gen)<conectividad)
+               {
+                    conexion[i].push_back(j);
+                    conexion[j].push_back(i);
+                    cout<<i<<"  "<<j<<"    ";
+               }
+
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+
+        for (i = 0; i<N; i++)
+        {
+           tamano = conexion[i].size();
+           for(j = 0; j<tamano; j++)
+           {
+               cout<<conexion[i][j]<<"  ";
+           }
+           cout<<endl;
+        }
+
+
+    return(0);
+}
